840c909a29184ce8c40cb65050bc63753eef4d7f
Kahina Zaidi <kahina.zaidi@ens.uvsq.fr>
2024-09-25 09:48:23 +0200

    Ajout des constructeurs

--- Fraction.java
+++ Fraction.java
@@ -1,9 +1,56 @@
-
 public class Fraction {
+    private int numerator;
+    private int denominator;
 
-    public String toString() {
-        return "je suis une fraction.";
-
+    public Fraction(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
     }
 
+    public Fraction(int numerator) {
+        this(numerator, 1);
+    }
+
+    public Fraction() {
+        this(0, 1);
+    }
+
+    @Override
+    public String toString() {
+        return numerator + "/" + denominator;
+    }
+
+    public int getNumerator() {
+        return numerator;
+    }
+
+    public int getDenominator() {
+        return denominator;
+    }
+
+    public double doubleValue() {
+        return (double) numerator / denominator;
+    }
+
+    public Fraction add(Fraction other) {
+        int newNumerator = this.numerator * other.denominator + other.numerator * this.denominator;
+        int newDenominator = this.denominator * other.denominator;
+        return new Fraction(newNumerator, newDenominator);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Fraction other = (Fraction) obj;
+        return this.numerator * other.denominator == other.numerator * this.denominator;
+    }
+
+    public int compareTo(Fraction other) {
+        return Integer.compare(this.numerator * other.denominator, other.numerator * this.denominator);
+    }
+
+    public static final Fraction ZERO = new Fraction(0, 1);
+    public static final Fraction UN = new Fraction(1, 1);
 }
+
--- Main.java
+++ Main.java
@@ -1,8 +1,41 @@
 public class Main {
-
     public static void main(String[] args) {
-        Fraction fraction = new Fraction();
-        System.out.println(fraction.toString());
-    }
+        // Test des constructeurs
+        Fraction f1 = new Fraction(3, 4);  // 3/4
+        Fraction f2 = new Fraction(5);     // 5/1
+        Fraction f3 = new Fraction();      // 0/1
 
+        // Test toString()
+        assert f1.toString().equals("3/4");
+        assert f2.toString().equals("5/1");
+        assert f3.toString().equals("0/1");
+
+        // Test des constantes ZERO et UN
+        assert Fraction.ZERO.toString().equals("0/1");
+        assert Fraction.UN.toString().equals("1/1");
+
+        // Test des getters
+        assert f1.getNumerator() == 3;
+        assert f1.getDenominator() == 4;
+
+        // Test de doubleValue()
+        assert f1.doubleValue() == 0.75;
+        assert f2.doubleValue() == 5.0;
+
+        // Test de l'addition
+        Fraction result = f1.add(new Fraction(1, 4));  // 3/4 + 1/4 = 1/1
+        assert result.toString().equals("16/16");  // Simplification non implémentée dans cet exemple
+
+        // Test d'égalité
+        Fraction f4 = new Fraction(2, 4);  // équivalent à 1/2
+        assert f4.equals(new Fraction(1, 2));
+
+        // Test de comparaison
+        assert f1.compareTo(new Fraction(1, 2)) > 0;   // 3/4 est plus grand que 1/2
+        assert f1.compareTo(new Fraction(3, 4)) == 0;  // 3/4 est égal à 3/4
+        assert f1.compareTo(new Fraction(5, 4)) < 0;   // 3/4 est plus petit que 5/4
+
+        System.out.println("Tous les tests sont passés avec succès !");
+    }
 }
+
